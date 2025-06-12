bd2f9502487a8c5ccd9be4c0ecfcb0e53aeeffdf
Anh Kiet Vo <voanhkiet261192@gmail.com>
2025-06-12 13:48:08 +0700

    Frontend

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -12,14 +12,16 @@ function GalleryPage(){
   .catch((error)=>console.error("Error fetchin paintings:", error));
  },[]);
  return(
-  <div>
+  <div className="painting-container shadow-lg p-4">
     <h1>Gallery 🖼️</h1>
     <ul className="painting-gallery">
       {paintings.map((painting)=>(
         <li className="painting-card" key={painting._id}>
           <h3>{painting.title}</h3>
-          <img loading="lazy" className="rounded-lg shadow-lg hover:scale-105 transition" src={painting.image} alt={painting.title} />
-          <p>Price: ${painting.price}</p>
+          <img loading="lazy" className="w-full h-48 object-cover" src={painting.image} alt={painting.title} />
+          <h2 className="mt-2 font-bold">{painting.title}</h2>
+          <p className="text-gray-600">Price: ${painting.price}</p>
+
         </li>
       ))}
     </ul>
