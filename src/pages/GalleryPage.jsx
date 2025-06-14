063b4103ebd1891a72f9b209ccbd5a27a38e7d11
Anh Kiet Vo <voanhkiet261192@gmail.com>
2025-06-14 11:18:44 +0700

    Frontend

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,23 +1,32 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import "./GalleryPage.css"
-
+import Modal from "react-modal"; // Import modal package
+Modal.setAppElement("#root"); // Required for accessibility
 
 function GalleryPage(){
  const [paintings,setPaintings] = useState([]);
-
+  const [selectedPainting, setSelectedPainting] = useState(null);
  useEffect(()=>{
   axios.get("https://backend-ybh5.onrender.com/paintings")
   .then((response)=>setPaintings(response.data))
   .catch((error)=>console.error("Error fetchin paintings:", error));
  },[]);
+
+   const openModal = (painting) => {
+    setSelectedPainting(painting);
+  };
+
+  const closeModal = () => {
+    setSelectedPainting(null);
+  };
  return(
   <div className="painting-container shadow-lg p-4">
     <h1>Gallery 🖼️</h1>
     <ul className="painting-gallery">
       {paintings.map((painting)=>(
-        <li className="painting-card" key={painting._id}>
-          <h3>{painting.title}</h3>
+        <li className="painting-card" key={painting._id} onClick={()=>openModal(painting)} >
+          <h2 className="mt-2 font-bold">{painting.title}</h2>
           <img loading="lazy" className="w-full h-48 object-cover" src={painting.image} alt={painting.title} />
           <h2 className="mt-2 font-bold">{painting.title}</h2>
           <p className="text-gray-600">Price: ${painting.price}</p>
@@ -25,6 +34,17 @@ function GalleryPage(){
         </li>
       ))}
     </ul>
+     {/* Lightbox Modal */}
+      <Modal isOpen={selectedPainting !== null} onRequestClose={closeModal} className="modal" overlayClassName="overlay">
+        {selectedPainting && (
+          <div className="flex flex-col items-center">
+            <img src={selectedPainting.image} alt={selectedPainting.title} className="max-w-full max-h-full object-contain" />
+            <h2 className="mt-4 text-xl font-bold">{selectedPainting.title}</h2>
+            <p className="text-gray-600">${selectedPainting.price}</p>
+            <button onClick={closeModal} className="mt-4 p-2 bg-red-500 text-white rounded">Close</button>
+          </div>
+        )}
+      </Modal>
   </div>
   );
 }
