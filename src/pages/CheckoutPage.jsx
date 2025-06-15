10f57fe6b2d63ee2160b7b845c972cc917377329
Anh Kiet Vo <voanhkiet261192@gmail.com>
2025-06-15 12:35:37 +0700

    Frontend

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "./CheckoutPage.css";
 
 const CheckoutPage = () => {
     const [cart, setCart] = useState([]);
