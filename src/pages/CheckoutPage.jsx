0f7556bc1043f49ad51aa00b4fc161667a583edc
Anh Kiet Vo <voanhkiet261192@gmail.com>
2025-06-15 14:50:22 +0700

    Frontend

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,17 +1,26 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./CheckoutPage.css";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
 const CheckoutPage = () => {
     const [cart, setCart] = useState([]);
    const navigate = useNavigate();
+   const stripePromise = loadStripe("pk_test_51RaAjT2cPGYHJMkwTMvHJlXkP9IUgDJFrtJoD9pZUPzcLff3xDk3KIYAOsQI8GexOA7yvGF93BTgXnWT5dy9ce2a00jgYIjf5i");
   const [inputValue, setInputValue] = useState("");
        useEffect(() => {
            const savedCart = JSON.parse(localStorage.getItem("cart")) || [];
            setCart(savedCart);
    }, []);  
  
+   const CheckoutForm = () => {
+    return(
+      <Elements stripe={stripePromise}>
 
+      </Elements>
+    );
+   };
   const handleChange = (e) => {
     setTimeout(()=>setInputValue(e.target.value), 300)
   };
@@ -21,7 +30,10 @@ const CheckoutPage = () => {
       alert("Your cart is empty!");
       return;
     }
-   
+    if (!inputValue) {
+    alert("⚠️ Please select a payment method!");
+    return;
+    }
     setCart([]); // ✅ Clear cart after purchase
     localStorage.removeItem("cart"); // ✅ Remove stored cart data
     navigate("/confirmation"); // Redirect after purchase
@@ -40,7 +52,14 @@ const CheckoutPage = () => {
       <h2 className="text-xl font-bold mt-4">Your Details:</h2>
       <input type="text" name="name" placeholder="Name" onChange={handleChange} />
       <input type="text" name="address" placeholder="Address" onChange={handleChange} />
-      <input type="text" name="payment" placeholder="Payment Method" onChange={handleChange} />
+
+      <select name="payment" className="payment-method" onChange={handleChange}>
+      <option value="">Select Payment Method</option>
+      <option value="credit-card">💳 Credit Card</option>
+      <option value="paypal">💰 PayPal</option>
+      <option value="bank-transfer">🏦 Bank Transfer</option>
+      </select>
+
 
       <button className="checkout-btn mt-4" onClick={handleCheckout}>
         ✅ Confirm Purchase
