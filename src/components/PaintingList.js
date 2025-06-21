b9b389084046925400fdb7956c440707fc5a164e
voanhkiet <voanhkiet261192@gmail.com>
2025-06-21 15:16:07 +0700

    refactor: update PaintingList component to include painting filtering and rendering

--- src/components/PaintingList.js
+++ src/components/PaintingList.js
@@ -6,5 +6,28 @@ const PaintingList = () => {
     const [paintings, setPaintings] = useState([]);
     const [query, setQuery] = useState("");
 
-    
+    useEffect(()=>{
+        axios.get("https://backend-e0sb.onrender.com/paintings")
+            .then(res => setPaintings(res.data))
+            .catch(err => console.error(err));
+    },[])
+
+    const filteredPaintings = paintings.filter(p => 
+        p.title.toLowerCase().includes(query.toLowerCase())
+    );
+
+    return (
+        <div>
+            <SearchBar setQuery={setQuery} />
+            {filteredPaintings.map(painting =>(
+                <div key={painting.id}>
+                    <h3>{painting.id}</h3>
+                    <img src={painting.imageUrl} alt={painting.title} />
+                    <p>${painting.price}</p>
+                </div>
+            ))}
+        </div>
+    )
 }
+
+export default PaintingList;
