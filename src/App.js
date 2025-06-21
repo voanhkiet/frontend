4a084b4f22f33a7666df0e508f9cbdbd0b35f492
voanhkiet <voanhkiet261192@gmail.com>
2025-06-21 17:52:46 +0700

    update home.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import PaintingList from './components/PaintingList';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-        <h1>🎨 Painting Store</h1>
-        <PaintingList/>
+      <Home />
     </div>
   );
 }
