69ceb1bf19c856ceca207c458b4a2bbcb6c2b63e
voanhkiet <voanhkiet261192@gmail.com>
2025-06-21 18:09:46 +0700

    fix all

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PaintingList from "../components/PaintingList";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>🎨 Paintin Store</h1>
+            <PaintingList />
+        </div>
+    );
+};
+
+export default Home;
