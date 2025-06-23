9ec7139764b1ecdef34c0bc6df808fa71ee0d83b
voanhkiet <voanhkiet261192@gmail.com>
2025-06-23 15:00:22 +0700

    updatecart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,24 @@
 import { useSelector } from "react-redux";
 const Cart = () => {
+  const cart = useSelector((state) => state.cart.items);
+  const total = cart.reduce((sum, item) => sum + item.price, 0);
+  const itemCount = cart.length;
   return (
-    <div>Your cart goes here</div>
+    <div>
+      <div classname="cart-items">
+        {cart.map(item =>(
+          <div key={item._id}>
+            <h4>{item.title}</h4>
+            <p>${item.price}</p>
+          </div>
+        ))}
+      </div>
+      <div className="cart-summary">
+      <h3>🧾 Cart Summary</h3>
+      <p>Items: ${itemCount}</p>
+      <p>Total: ${total.toFixed(2)}</p>
+      </div>
+    </div>
   );
 };
 
